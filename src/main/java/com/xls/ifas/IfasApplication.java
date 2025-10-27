a2b1452a2c4a040aa0bbdd2480808cf4a589f7c0
Xls-technology <xlstechnology07@gmail.com>
2025-10-28 00:06:55 +0530

    first commit

---
+++ src/main/java/com/xls/ifas/IfasApplication.java
@@ -0,0 +1,13 @@
+package com.xls.ifas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xls/ifas/IfasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xls.ifas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
